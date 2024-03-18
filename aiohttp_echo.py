c8ebc971d976d9d37b80fabbf6bf05f30a6f7cf2
imyhxy <imyhxy@gmail.com>
2024-03-18 20:04:47 +0800

    Update aiohttp_echo.py

--- aiohttp_echo.py
+++ aiohttp_echo.py
@@ -1,16 +1,69 @@
+import os
+
+import chardet
 from aiohttp import web
 
+TEMP_DIR = "temp"
+if not os.path.exists(TEMP_DIR):
+    os.mkdir(TEMP_DIR)
 
-async def hello(request):
+
+def print_base(request: web.Request) -> None:
     print("Request:", repr(request))
     print("Headers:", dict(request.headers))
-    print("Body   :", await request.text())
+
+
+async def general(request: web.Request) -> web.Response:
+    print_base(request)
+    if request.can_read_body:
+        content = await request.read()
+        result = chardet.detect(content)
+        if result["confidence"] >= 0.95:
+            content = content.decode(result["encoding"])
+        print("Content:", content[:200])
 
     return web.Response()
 
 
-app = web.Application()
-app.add_routes([web.get("/{tail:.*}", hello)])
-app.add_routes([web.post("/{tail:.*}", hello)])
+async def alarm_synch_alarm_infos(request: web.Request) -> web.Response:
+    print_base(request)
+    assert request.content_type == "application/x-www-form-urlencoded"
+    print("Content:", (await request.text())[:200])
+    return web.Response()
 
-web.run_app(app)
+
+async def file_upload(request: web.Request) -> web.Response:
+    print_base(request)
+    assert request.content_type == "multipart/form-data"
+    reader = await request.multipart()
+    field = await reader.next()
+    remain = 200
+    while field is not None:
+        if field.name == "file":
+            print("Content: ", end="")
+            with open(os.path.join(TEMP_DIR, field.filename), "wb") as f:
+                while True:
+                    chunk = await field.read_chunk()
+                    if not chunk:
+                        break
+                    f.write(chunk)
+                    if remain > 0:
+                        print(chunk[:remain], end="")
+                        remain = remain - len(chunk)
+            print()
+        else:
+            print(f"Field: {field.name}, {field.filename}")
+
+        field = await reader.next()
+
+    return web.Response()
+
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.add_routes([web.post("/alarm/synchAlarmInfos", alarm_synch_alarm_infos)])
+    app.add_routes([web.post("/uploadFile", file_upload)])
+    app.add_routes([web.get("/{tail:.*}", general)])
+    app.add_routes([web.post("/{tail:.*}", general)])
+
+    web.run_app(app)
--- http_echo.py
+++ http_echo.py
@@ -14,7 +14,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         content_length = int(self.headers["Content-Length"])
-        post_data = self.rfile.read(content_length).decode("utf-8")
+        post_data = self.rfile.read(content_length)
 
         self._set_response()
         self.wfile.write(b"Success! This is a simple HTTP server")
@@ -26,10 +26,12 @@ class RequestHandler(BaseHTTPRequestHandler):
         print(f"Path: {self.path}")
         print(f"Headers: {self.headers}")
         if post_data:
+            if len(post_data) > 200:
+                post_data = post_data[:200]
             print(f"Post Data: {post_data}")
 
 
-def run(server_class=HTTPServer, handler_class=RequestHandler, port=8800):
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
     print(f"Starting server on port {port}")
